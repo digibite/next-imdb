4117ec4de20f1119137ffc50ac0b7ea33c1588b0
ig <ig@igs-MacBook-Pro.lan>
2024-05-19 05:21:32 -0700

    Add Search box

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 function MenuItem({ title, address, Icon }) {
   return (
-    <Link href="address" className="hover:text-amber-500">
+    <Link href={address} className="hover:text-amber-500">
       <Icon className="text-2xl sm:hidden" />
       <p className="uppercase hidden sm:inline text-sm transition-colors duration-300">
         {title}
